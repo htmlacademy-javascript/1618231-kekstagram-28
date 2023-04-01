b6fdb2a32693f44d21f0bb5a0db490f7d8b0b169
Dmitry1964 <nosdm64@gmail.com>
2023-04-01 08:59:00 +0300

    Добавляет ДЗ Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 import { request } from './api.js';
 import {onSucces, onError} from './pictures.js';
+import { showImgFilterSection } from './filters.js';
 import './event-pictures.js';
 import './upload-file.js';
 import './form-validation.js';
 import './effects.js';
 
 
-window.addEventListener('DOMContentLoaded', request(onSucces, onError, 'GET'));
-
-
+window.addEventListener('DOMContentLoaded', () => {
+  request(onSucces, onError, 'GET');
+  showImgFilterSection();
+});
